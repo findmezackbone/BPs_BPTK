f30da4619d51b598100c2bd1126e4967efa7d3a9
jingran <2955594048@qq.com>
2024-04-16 16:23:32 +0800

    更新论文

--- Python/onlyfortest copy.py
+++ Python/onlyfortest copy.py
@@ -9,11 +9,17 @@ from scipy import stats
 j = 0
 time = np.arange(0,75,0.005) #七十五个小时的时间戳
 a = np.array([[16,5,5]])
-_,bps,bpsg = BPS_BPTK_MultiParas(t = time,volunteer_ID =1, paras = a ,mode = '63')
+blood,bps,bpsg = BPS_BPTK_MultiParas(t = time,volunteer_ID =1, paras = a ,mode = '63')
 
 plt.plot(time,bps[0],label = 'bps')
 
 plt.plot(time,bpsg[0],label = 'bpsg')
+
+plt.legend()
+
+plt.show()
+
+plt.plot(time,blood[0]*1000,label = 'blood')
 plt.legend()
 
 plt.show()
