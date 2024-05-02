7ca5d7f587a8781b9867256c3e0b6c63b80b8a61
jingran <2955594048@qq.com>
2024-05-02 22:44:00 +0800

    5.2时间信息的神经网络

--- Python/onlyfortest.py
+++ Python/onlyfortest.py
@@ -5,31 +5,21 @@ import sys
 import time
 sys.path.append("Python") 
 from BPS_init_function_MultiParas_copy import BPS_BPTK_MultiParas
-
 from scipy import stats
+true_index = np.loadtxt(open("Documents\Volunteer_data.csv"),delimiter=",",skiprows=1,usecols=[1,2,3,4]) #真实受试者的尿液数据
+
+true_index =true_index[0:17,0]
+
+timeline = np.arange(0,75,0.005)
+a = np.ones(np.shape(timeline)).flatten()
+
+plt.plot(timeline, a)
+
+plt.scatter(true_index, np.ones(np.shape(true_index)).flatten() )
+plt.show()
 j = 0
 time1 = np.arange(0,75,0.005) #七十五个小时的时间戳
 mean1 = 17.28
 mean2 = 6.39
 mean3 = 5.7
 
-X1 = stats.truncnorm(-2, 2.3, loc=mean1, scale=5)
-x1 = X1.rvs(size = 6,random_state = 43)
-X2 = stats.truncnorm(-1, 1.3, loc=mean2, scale=5)
-x2 = X2.rvs(size = 5,random_state = 43)
-X3 = stats.truncnorm(-1, 1.2, loc=mean3, scale=5)
-x3 = X3.rvs(size = 5,random_state = 43)
-
-a = np.array(list(itertools.product(x1, x2, x3)))
-
-start = time.perf_counter()
-
-data = BPS_BPTK_MultiParas(t = time1,volunteer_ID =1, paras = a ,mode = '63')
-
-end = time.perf_counter()
-
-runTime = end - start
-print("运行时间：", runTime)
-paras = np.array(list(itertools.product(x1, x2, x3)))
-#0.10864299066655803333333333333333s
-#18.16208s
--- ssa.py
+++ ssa.py
@@ -16,29 +16,7 @@ sys.path.append("Python")
 import matplotlib.pyplot as plt
 import keyboard
 
-def standard_transform(x):
-    # 计算每个特征的均值
-    mean = x.mean(dim=0)
-    # 计算每个特征的标准差
-    std = x.std(dim=0, unbiased=False)  # unbiased=False 相当于 numpy 的 ddof=0
-    # 避免使用0的标准差
-    std[std == 0] = 1
-    # 进行标准化转换
-    x = (x - mean) / std
-    return x,mean,std
+a = np.array([[1,2,3],[4,5,6]])
+b = a.flatten()
 
-def inverse_transform(x, mean, std):
-    # 将标准化后的张量乘以标准差，然后加上均值
-    x = x * std + mean
-    return x
-
-c= torch.tensor([[1.555,2,3],[2,3,4],[7,21,4]]).float()
-
-m,mean,std  = standard_transform(c)
-y = inverse_transform(m, mean, std)
-
-
-print(type(m))
-print(m)
-print(type(y))
-print(y)
+print(b)
